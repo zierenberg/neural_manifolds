1d9f8b96aca2be195f5caa71efd5e00db01512a5
Johannes Zierenberg <johannes.zierenberg@gmail.com>
2022-11-04 15:32:37 +0100

    updated API for inhomogenous poisson spikes

--- neural_manifolds/test_data/spikes.py
+++ neural_manifolds/test_data/spikes.py
@@ -1,37 +1,39 @@
+from bisect import bisect
 import numpy as np
 from ..utils import set_seed
 
 
 @set_seed()
-def inhomogenous_poisson_spikes(rates, dt):
+def inhomogenous_poisson_spikes(rate_object):
     """
-    generate spikes from `n` (number of rows in rates) time-continuous point processes with time-dependent rates `rate_i` (ith row of rates)
+    generate spikes from `n` (number of rows in rate_object["rates"]) time-continuous point processes with time-dependent rates `rate_i` (ith row of rate_object["rates"])
 
     Parameters
     ----------
-    rates :  2D array, shape (n_components, n_measurements)
-        First dimension are the components/neurons, second dimension are the measurements/time.
-    dt : float
-        time step
+    rate_object: dictionary with elements "rates" and "times". "rates" is a 2D array, shape (n_components, n_measurements) where 
+        first dimension is neurons and second dimension is the time-dependent rate. "times" is a sorted 1D array of discrete times.
 
     Returns
     -------
     signals : 2D array, shape (id_neuron, time_spike)
     """
     # start with empty array
-    rates = np.array(rates)
-    t_max = rates.shape[1] * dt
+    #rates = np.array(rates)
+    times = rate_object["times"]
+    rates = rate_object["rates"]
 
     # Iterate over the "neurons"
     spikes = []
     for i, rate in enumerate(rates):
         rate_max = rate.max()
-        # Can someone explain in more detail what this does?
-        time = 0
-        while time < t_max:
-            time += np.random.exponential() / rate_max
-            if time < t_max:
-                if np.random.random() < rate[int(time / dt)] / rate_max:
-                    spikes.append([i, time])
+        # advance time continously as if it was a Poisson process with rate_max
+        time = times[0] + np.random.exponential() / rate_max
+        while time < times[-1]:
+            # accept new time as a spike with probability rate(time)/rate_max
+            time_index = bisect(times,time)-1
+            if np.random.random() < rate[time_index] / rate_max:
+                spikes.append([i, time])
 
+            time += np.random.exponential() / rate_max
+            
     return np.array(spikes)
